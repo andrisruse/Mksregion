6a89f8e44fa169dfda36b86df104c9529901d165
andrisruse <67731361+andrisruse@users.noreply.github.com>
2022-10-29 12:38:02 +0800

    Create Mksregion.php

---
+++ src/Mksregion.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+ * This file is part of the Mksregion package.
+ *
+ * (c) Andris Ruse <andrisruse.com | andrisruse@gmail.com>
+ *
+ */
+
+namespace andrisruse\Mksregion;
+
+use App\Models\Province;
+use App\Models\Regency;
+use App\Models\District;
+use App\Models\Village;
+
+class IndoRegion
+{
+    public function getProvinces()
+    {
+        # code...
+    }
+
+    public function searchProvinces($name = '')
+    {
+        # code...
+    }
+
+    public function findProvince($key = '', $value = '')
+    {
+        # code...
+    }
+}
